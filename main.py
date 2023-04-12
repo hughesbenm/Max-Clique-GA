aa4c246fa1d55c2f92d91d5a49c7aba60a79b6d2
Ben Hughes <hughesbenm@gmail.com>
2023-04-12 11:49:18 -0500

    revelations about fitness function, degree based

--- main.py
+++ main.py
@@ -38,8 +38,8 @@ def gen_graph(nodes, clique_size, edge_prob):
   return G
 
 # WRONG, NEED TO CONSIDER SUBGRAPH NOT FULL GRAPH
-def get_fitness(chromosome, nodes, G, k, experimental = False):
-  list = chromosome_to_node_list(chromosome, nodes)
+def get_fitness(chromosome, G, k, experimental = True):
+  list = chromosome_to_node_list(chromosome)
 
   S = G.subgraph(list)
 
@@ -70,20 +70,20 @@ def gen_population(pop_size, nodes):
     pop[i] = gen_chromosome(nodes)
   return pop
 
-def chromosome_to_node_list(chromosome, nodes):
+def chromosome_to_node_list(chromosome):
   list = []
-  for i in range(nodes):
+  for i in range(len(chromosome)):
     if chromosome[i] == 1:
       list.append(i)
   return list
 
 # Takes in list of chromosomes, returns list of randomly selected parents
-def rank_select(pop, pop_size, nodes, G, k, elites):
-  fitnesses = [get_fitness(i, nodes, G, k) for i in pop]
+def rank_select(pop, G, k, elites):
+  fitnesses = [get_fitness(i, G, k) for i in pop]
   ranked = [sorted(fitnesses).index(i) + 1 for i in fitnesses]
   sum_ranks = sum(ranked)
-  probabilities = [ranked[i] / sum_ranks for i in range(pop_size)]
-  parents = random.choices(pop, probabilities, k = pop_size - elites)
+  probabilities = [ranked[i] / sum_ranks for i in range(len(pop))]
+  parents = random.choices(pop, probabilities, k = len(pop) - elites)
   return parents
 
 def uniform_cross(parents):
@@ -105,8 +105,8 @@ def single_mutate(chromosome, mutation_rate):
     chromosome[rand_index] = (chromosome[rand_index] + 3) % 2
   return chromosome
 
-def find_elites(pop, nodes, num_elites, G, k):
-  fitnesses = [get_fitness(i, nodes, G, k) for i in pop]
+def find_elites(pop, num_elites, G, k):
+  fitnesses = [get_fitness(i, G, k) for i in pop]
   ranked = [sorted(fitnesses).index(i) + 1 for i in fitnesses]
   ranked_sorted = sorted(ranked, reverse=True)
   elites = []
@@ -121,40 +121,52 @@ def SGA(nodes, k, edge_prob, pop_size, num_elites, mutation_rate):
   G = gen_graph(nodes, k, edge_prob)
 
   pop = gen_population(pop_size, nodes)
+  gens = 0
+  
+  while gens < 1:
+    parents = rank_select(pop, G, k, num_elites)
 
-  parents = rank_select(pop, pop_size, nodes, G, k, num_elites)
+    children = uniform_cross(parents)
 
-  children = uniform_cross(parents)
+    mutated_children = [single_mutate(i, mutation_rate) for i in children]
 
-  mutated_children = [single_mutate(i, mutation_rate) for i in children]
+    elites = find_elites(pop, num_elites, G, k)
 
-  elites = find_elites(pop, nodes, num_elites, G, k)
+    next_pop = copy.deepcopy(pop)
+    for i in range(pop_size - num_elites):
+      next_pop[i] = mutated_children[i]
+    for i in range(num_elites):
+      next_pop[pop_size - i - 1] = elites[i]
 
-  next_pop = copy.deepcopy(pop)
-  for i in range(pop_size - num_elites):
-    next_pop[i] = mutated_children[i]
-  for i in range(num_elites):
-    next_pop[pop_size - i - 1] = elites[i]
+    pop = next_pop
+    
+    print("gen ", gens, " best fitness: ", get_fitness(elites[0], G, k))
+    gens += 1
+
+    nx.draw(G, with_labels=True)
+    plt.show()
+    nx.draw(G.subgraph(chromosome_to_node_list(elites[0])), with_labels=True)
+    plt.show()
 
 
 
 
 random.seed()
 
-NODES = 100
-K = 17
+NODES = 30
+K = 5
 EDGE_PROB = 0.2
 POP_SIZE = 50
 NUM_ELITES = 2
 MUTATION_RATE = 1
 
-SGA(NODES, K, EDGE_PROB, POP_SIZE, NUM_ELITES, MUTATION_RATE)
+# SGA(NODES, K, EDGE_PROB, POP_SIZE, NUM_ELITES, MUTATION_RATE)
 
+G = gen_graph(NODES, K, EDGE_PROB)
 
-# nx.draw(G, with_labels=True) 
-# plt.show() 
-# nx.draw(G.subgraph(list), with_labels=True)
-# plt.show()
+chromo = gen_chromosome(NODES)
 
-# print("experimental fitness: ",  get_fitness(chromosome, NODES, G, True, K))
-# print("normal fitness: ", get_fitness(chromosome, NODES, G, False, K))
+print(get_fitness(chromo, G, K))
+
+nx.draw(G.subgraph(chromosome_to_node_list(chromo)), with_labels=True)
+plt.show()
